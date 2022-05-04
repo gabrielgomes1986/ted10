55a26e7b5ab116bbb973377f656a4efc578d0cde
gabrielgomes1986 <103393275+gabrielgomes1986@users.noreply.github.com>
2022-05-04 20:39:43 -0300

    Add files via upload

---
+++ Ted10/ted10.a.py
@@ -0,0 +1,8 @@
+#Faça um algoritmo para ler 20 números e armazenar em um vetor. Após a leitura total dos 20 números, o algoritmo deve escrever esses 20 números lidos na ordem inversa.
+
+for numeros in range(21):
+
+    print(numeros)
+
+for decrescente in range(20, -1, -1): 
+    print(decrescente)
